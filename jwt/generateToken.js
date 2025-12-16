541708473c7e116f418aac6e4ad128e7013678a4
officialrathore <bharatkrahore8952@gmail.com>
2025-12-17 02:09:23 +0530

    updated

--- jwt/generateToken.js
+++ jwt/generateToken.js
@@ -1,11 +1,16 @@
 import jwt from "jsonwebtoken";
 
 export const createTokenAndSaveCookie = (userId, res) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "30d" });
-    
-    res.cookie("jwt", token, {
-        httpOnly: true,
-        secure: true, 
-        sameSite: "strict", 
-    });
-}
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: true,          
+    sameSite: "none",      
+    path: "/",             
+  });
+
+  return token;
+};
